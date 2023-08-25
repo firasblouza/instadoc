ca0c2fc67c3f7ddea56b6929f6f006d65d118e89
Firas Blouza <firas.blouza1@gmail.com>
2023-08-26 00:20:19 +0100

    Update allowedOrigins

--- server/src/config/allowedOrigins.js
+++ server/src/config/allowedOrigins.js
@@ -1,12 +1,3 @@
-const allowedOrigins = [
-  "http://127.0.0.1:3500",
-  "http://localhost:3500",
-  "http://localhost:3000",
-  "http://127.0.0.1:3000",
-  "http://localhost:5173",
-  "http://127.0.0.1:5173",
-  "https://instadoc-server.vercel.app/",
-  "https://instadoc.vercel.app/"
-];
+const allowedOrigins = ["https://instadoc.onrender.com"];
 
 module.exports = allowedOrigins;
