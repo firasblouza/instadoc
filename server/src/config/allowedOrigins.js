09c4354a29637bbf8d5d46c0f6801848358ff5ff
Firas Blouza <firas.blouza1@gmail.com>
2023-09-24 15:07:59 +0100

    Update allowed origins

--- server/src/config/allowedOrigins.js
+++ server/src/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-  "https://instadoc-server.vercel.app",
-  "https://instadoc.vercel.app"
+  "https://instadoc-server.vercel.app/",
+  "https://instadoc.vercel.app/"
 ];
 
 module.exports = allowedOrigins;
