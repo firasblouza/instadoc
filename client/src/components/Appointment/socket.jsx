0d5ed62985630e4393096520d5228a6ddcd2eebc
Firas Blouza <firas.blouza1@gmail.com>
2023-09-24 16:34:39 +0100

    Update socket.js

--- client/src/components/Appointment/socket.jsx
+++ client/src/components/Appointment/socket.jsx
@@ -3,6 +3,5 @@ import { io } from "socket.io-client";
 const URL = "https://instadoc-api.onrender.com";
 
 export const socket = io(URL, {
-  autoConnect: false,
-  transports: ["websocket"]
+  autoConnect: false
 });
